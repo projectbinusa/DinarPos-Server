cb4ec16b58a5c685cd3be03b195fe4aeaa0d79f9
NizarRestu <nizarrestuaji18@gmail.com>
2024-05-16 13:13:27 +0700

    update

--- src/main/java/com/template/eazypos/repository/PiutangRepository.java
+++ src/main/java/com/template/eazypos/repository/PiutangRepository.java
@@ -4,10 +4,15 @@ import com.template.eazypos.model.Hutang;
 import com.template.eazypos.model.Piutang;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PiutangRepository extends JpaRepository<Piutang , Long> {
     @Query(value = "SELECT h FROM Piutang h WHERE h.pelunasan <> '0'")
     List<Piutang> findAllPiutang();
+
+    @Query("SELECT h FROM Piutang h WHERE h.date BETWEEN :tglAwal AND :tglAkhir")
+    List<Hutang> findByTanggalBetween(@Param("tglAwal") Date tglAwal, @Param("tglAkhir") Date tglAkhir);
 }
