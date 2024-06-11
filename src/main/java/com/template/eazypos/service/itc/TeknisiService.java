dac2458d2836d1e76221ba98898ee7c83ff3348e
niscitacan12 <citacandrarini@gmail.com>
2024-06-11 09:30:58 +0700

    pagination getall teknisi

--- src/main/java/com/template/eazypos/service/itc/TeknisiService.java
+++ src/main/java/com/template/eazypos/service/itc/TeknisiService.java
@@ -3,15 +3,15 @@ package com.template.eazypos.service.itc;
 import com.template.eazypos.dto.TeknisiDTO;
 import com.template.eazypos.exception.BadRequestException;
 import com.template.eazypos.exception.NotFoundException;
-import com.template.eazypos.model.Pengguna;
-import com.template.eazypos.model.Poin;
-import com.template.eazypos.model.Salesman;
-import com.template.eazypos.model.Teknisi;
+import com.template.eazypos.model.*;
 import com.template.eazypos.repository.PenggunaRepository;
 import com.template.eazypos.repository.PoinRepository;
 import com.template.eazypos.repository.TeknisiRepository;
-import com.template.eazypos.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -100,6 +100,21 @@ public class TeknisiService {
             return res;
         } catch (Exception e) {
             return null;
+        }
+    }
+
+    public Page<Teknisi> getAllWithPagination(Long page, Long limit, String sort, String search) {
+        Sort.Direction direction = Sort.Direction.ASC;
+        if (sort.startsWith("-")) {
+            sort = sort.substring(1);
+            direction = Sort.Direction.DESC;
+        }
+
+        Pageable pageable = PageRequest.of(Math.toIntExact(page - 1), Math.toIntExact(limit), direction, sort);
+        if (search != null && !search.isEmpty()) {
+            return teknisiRepository.findAllByKeyword(search, pageable);
+        } else {
+            return teknisiRepository.findAll(pageable);
         }
     }
 }
