a2d7162ea80936b1ae92ac605c7adaa3f4284931
niscitacan12 <citacandrarini@gmail.com>
2024-08-13 11:30:28 +0700

    update dan perbaikan

--- src/main/java/com/template/eazypos/repository/ServiceRepository.java
+++ src/main/java/com/template/eazypos/repository/ServiceRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
